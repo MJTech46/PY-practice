f853a9b41cab3ac8a5921398e33fd4d47f550acd
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-10-17 15:33:29 +0530

    Python with braces: Bython

---
+++ Bython/main.py
@@ -0,0 +1,17 @@
+def say_hello() -> None:
+    print("Hello world")
+
+def say_Working() -> None:
+    print("\nBython is working\n")
+
+def say_bye() -> None:
+    print("Bye bye")
+
+if __name__ == "__main__":
+    say_hello()
+    say_Working()
+    say_bye()
+
+## example usage ##
+# python ./scripts/py2by main.py
+# python ./scripts/bython main.by
