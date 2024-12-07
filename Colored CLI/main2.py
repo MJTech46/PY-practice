eafbe737f3f88ecc3f827284cb20c930cd19d45a
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-12-07 21:47:02 +0530

    CLI colors: color them!

---
+++ Colored CLI/main2.py
@@ -0,0 +1,9 @@
+# install colorama
+
+from colorama import Fore, Back, Style
+
+print(Fore.RED + "This is red text")
+print(Fore.GREEN + "This is green text")
+print(Back.YELLOW + "This is with a yellow background")
+print(Style.BRIGHT + "Bright text style")
+print(Style.RESET_ALL + "Back to normal style\n")
---
+++ Colored CLI/main3.py
@@ -0,0 +1,7 @@
+# install termcolor
+
+from termcolor import colored
+
+print(colored("This is red text", "red"))
+print(colored("This is green text", "green"))
+print(colored("This has a blue background", "white", "on_blue"))
